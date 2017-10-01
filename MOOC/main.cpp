309066dcb4decdca757373582d4bc142f538f8fa
Saif <awainia@gmail.com>
2017-10-01 23:16:35 +0200

    first pointer example

---
+++ MOOC/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+    
+using namespace std;
+    
+int main()
+{
+    
+        int num = 3;
+        int *pNum = &num;
+
+        cout << pNum << endl;
+
+        return 0;
+}
+
